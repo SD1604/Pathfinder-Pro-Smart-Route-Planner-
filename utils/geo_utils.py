df551bf94ce4e415cd31b3f7e4edf1221a8632f0
Sushant Duggal <sushantduggal24@gmail.com>
2026-03-19 16:19:56 +0530

    Update geo_utils.py

--- utils/geo_utils.py
+++ utils/geo_utils.py
@@ -13,13 +13,13 @@ def get_nearest_edge(graph, lat, lon):
 def get_closest_node_from_edge(G, click_lat, click_lng, edge_tuple):
     u, v, key = edge_tuple
     
-    # Get coordinates for both nodes of the edge
+    # Get coordinates for both nodes
     node_u = G.nodes[u]
     node_v = G.nodes[v]
     
-    # Calculate simple squared distance (faster than square root for comparison)
+    # Calculate simple squared distance
     dist_u = (node_u['y'] - click_lat)**2 + (node_u['x'] - click_lng)**2
     dist_v = (node_v['y'] - click_lat)**2 + (node_v['x'] - click_lng)**2
     
-    # Return the ID of the closer node
+    # ID of the closer node
     return u if dist_u < dist_v else v
