c51351ab75889ae8c98f042d79b0149376b97d5a
Sushant <sushant@Sushants-MacBook-Air.local>
2026-03-25 21:27:20 +0530

    Minor Changes

--- backend/app.py
+++ backend/app.py
@@ -27,33 +27,9 @@ print("Map and Cache loaded successfully!")
 @app.route('/find_path', methods=['POST'])
 def find_path():
     data = request.json
-    
-    # # Extract coordinates from the request
-    # start_lat, start_lng = data['start'][0], data['start'][1]
-    # end_lat, end_lng = data['end'][0], data['end'][1]
 
-    # # Finding the nearest edges first then converting them into nodes
-    # start_edge = get_nearest_edge(G, start_lat, start_lng)
-    # end_edge = get_nearest_edge(G, end_lat, end_lng)
-    
-    # # 2. Find nearest nodes in the graph
-    # start_node = start_edge[0]  # Start node from the nearest edge
-    # end_node = end_edge[0]  # End node from the nearest edge
-
-    # t0 = time.time()
     start_coords = (data['start'][0], data['start'][1])
     end_coords = (data['end'][0], data['end'][1])
-
-    # # 1. Find nearest edges
-    # start_edge = ox.distance.nearest_edges(G, X=start_coords[1], Y=start_coords[0])
-    # end_edge = ox.distance.nearest_edges(G, X=end_coords[1], Y=end_coords[0])
-    # print(f"Nearest edges found in: {time.time() - t0:.4f}s")
-
-    # 2. pick the better node from each edge
-    # t1 = time.time()
-    # start_node = get_closest_node_from_edge(G, start_coords[0], start_coords[1], start_edge)
-    # end_node = get_closest_node_from_edge(G, end_coords[0], end_coords[1], end_edge)
-    # print(f"Nodes picked in: {time.time() - t1:.4f}s")
 
     # --- UI SYNC: START BENCHMARK ---
     ui_bench_start = time.time()
@@ -62,9 +38,6 @@ def find_path():
     start_node = get_nearest_node(tree, node_ids, start_coords[0], start_coords[1])
     end_node = get_nearest_node(tree, node_ids, end_coords[0], end_coords[1])
     # print(f"Nearest nodes found in: {time.time() - t1:.4f}s")
-
-    # # 3. Custom Dijkstra
-    # path_nodes, distance_meters = custom_dijkstra(G, start_node, end_node)
 
     # 3. A* Pathfinding
     # t2 = time.time()
