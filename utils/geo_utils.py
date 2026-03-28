89d0a10c6b9f013b0a5a930cb85a87e9a09a5818
Sushant Duggal <sushantduggal24@gmail.com>
2026-03-28 23:16:26 +0530

    Clean up geo_utils.py by removing commented code
    Removed unused functions for edge and node calculations.

--- utils/geo_utils.py
+++ utils/geo_utils.py
@@ -14,24 +14,3 @@ def get_nearest_node(tree, node_ids, lat, lon):
 
     _, index = tree.query([lat, lon])
     return node_ids[index]
-
-
-
-# def get_nearest_edge(graph, lat, lon):
-
-#     nearest_edge = ox.nearest_edges(graph, X=lon, Y=lat)
-#     return nearest_edge
-
-# def get_closest_node_from_edge(G, click_lat, click_lng, edge_tuple):
-#     u, v, key = edge_tuple
-    
-#     # Get coordinates for both nodes
-#     node_u = G.nodes[u]
-#     node_v = G.nodes[v]
-    
-#     # Calculate simple squared distance
-#     dist_u = (node_u['y'] - click_lat)**2 + (node_u['x'] - click_lng)**2
-#     dist_v = (node_v['y'] - click_lat)**2 + (node_v['x'] - click_lng)**2
-    
-#     # ID of the closer node
-#     return u if dist_u < dist_v else v
